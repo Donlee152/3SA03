310d274279b47773ff6f8fb641f51a323133e9b4
Donlee152/3SA03 <6110110152@psu.ac.th>
2020-08-31 20:55:43 +0700

    สามารถนับรอบและแสดงผลได้

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,26 +1,31 @@
-import React, { useState, useEffect,useRef } from 'react';
-
-export default function CharacterCard(props) {
-    const [active, setActive] = useState(false);
-    const attemptRef = useRef(props.attempt);
-
-    const activate = () => {
-        if(!active){
-            setActive(true)
-            props.activationHandler(props.value)
+import React, { Component } from 'react';
+export default class CharacterCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        active: false,
         }
     }
-
-    useEffect(() => {
-        if(attemptRef.current !=props.attempt){
-            setActive(false);
-            attemptRef.current = props.attempt
+       
+        activate = () => {
+            if(!this.state.active){
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
+            }
         }
-    })
+        componentDidUpdate(prevProps){
+            
+            if(prevProps.attempt != this.props.attempt){
+            this.setState({active: false})
+            }
+        }
 
-    const className = `card ${active ? 'activeCard' : ''}`
 
+    render(){
+        let className = `card ${this.state.active ? 'activeCard': ''}`
     return (
-        <div className={className} onClick={activate}>{props.value}</div>  
-    )
+       <div className={className} onClick={this.activate}>
+      {this.props.value}</div>
+ )
+ }
 }
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,48 +1,54 @@
-import React, { useState} from 'react';
+import React, { Component } from 'react';
+import CharacterCard from "./CharacterCard";
 import _ from 'lodash';
-
-import CharacterCard from './CharacterCard';
-
-const prepareStateFromWord = given_word => {
+const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
     return {
-        word,
-        chars,
-        attemp: 1,
-        guess: '',
-        completed: false
+    word,
+    chars,
+    attempt: 1,
+    guess: [],
+    completed: false
     }
-}
-
-export default function WordCard(props){
-
-    const [state, setState] = useState(prepareStateFromWord(props.value))
-
-    const activationHandler = c =>{
-        console.log(`${c} has been activated`)
-
-        let guess = state.guess + c
-        setState({...state, guess})
-
-        if(guess.length == state.word.length){
-            if(guess==state.word){
-                console.log('yeah!')
-                setState({...state, completed: true})
-            }else{
-                console.log('reset, next attemp')
-                setState({...state, guess: '',attempt: state.attempt + 1})
-            }
+   }
+   
+export default class WordCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+    activationHandler = (c) => {
+        
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        
+        if(guess.length == this.state.chars.length){
+        if(guess.join('').toString() == this.state.word){
+        this.setState({guess: [], completed: true})
+        }else{
+        this.setState({guess: [], attempt: this.state.attempt + 1})
         }
-    }
+    
+        }
+       }
 
-    return(
-        <div>
-            {
-                state.chars.map((c, i) => 
-                     <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
-                     )
-            }
-        </div>
-    )
-}
+ render(){	    
+            return(	       
+                <div className="App">	            
+                    {
+                    Array.from(this.state.chars).map((c, i) => <CharacterCard value = {c} key = {i} 
+                    
+                    attempt={this.state.attempt}
+                    activationHandler = {this.activationHandler} />
+                    )
+                    }
+
+                
+                <p>รอบที่ : {this.state.attempt}</p>
+                <p>{this.state.completed? "you win" : ""}</p>
+                </div>	           
+            );	       
+        }	   
+        
+    }    
